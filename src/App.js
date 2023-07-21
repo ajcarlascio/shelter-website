db8eb892cc451b53cf6705b6000cc0e5be2450e1
ajcarlascio <ajcarlascio@gmail.com>
2023-07-20 21:16:31 -0400

    Added app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Login } from "./Components/Login";
+import { Register } from "./Components/Register";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentForm, setForm] = useState("Login");
+
+  const toggleForm = (formName) => {
+    setForm(formName);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {currentForm === "Login" ? <Login onToggle={toggleForm} /> : <Register onToggle={toggleForm}/>}
     </div>
   );
 }
